7a568172d4c986216b81520272a022b7f1c3999f
slooooowpanda <akanybekov@gmail.com>
2016-11-16 01:52:52 +0500

    Holt method updated

--- esm/esm/Content/func/HoltMethod.js
+++ esm/esm/Content/func/HoltMethod.js
@@ -1,31 +1,30 @@
-﻿function f(data) {
-    var alpha = 0.1;
-    var beta = 0.2;
+﻿function f(data, params) {
+    var alpha = params.alpha;
+    var beta = params.beta;
     var level = 0;
     var trend = 0;
     result[0] = data[0];
     for (i = 0; i <= data.length; ++i) {
-        if (i == 0) {
+        if (i === 0) {
             level = data[0];
             trend = data[1] - data[0];
         }
-
+        var value;
         if (n >= data.length) {
             value = result[-1];
         } else {
             value = data[i];
-            last_level = level;
+            var lastLevel = level;
             level = alpha * value + (1 - alpha) * (level + trend);
-            trend = beta * (level - last_level) + (1 - beta) * trend;
-            result.push(level + trend)
+            trend = beta * (level - lastLevel) + (1 - beta) * trend;
+            result.push(level + trend);
         }
     }
 
-    return result;
+    return result[data.length - 1];
 }
 
 function getTrendKoef(parameters) {
-    trendKoefSglazh = 0.2;
-
+    var trendKoefSglazh = 0.2;
 }
 
