12745b3da0e55e7905b2816f930157853857ec9c
Даниил Бобыкин <hitroub@gmail.com>
2016-12-23 20:43:36 +0500

    очередной грязный хак

--- esm/esm/Controllers/TestController.cs
+++ esm/esm/Controllers/TestController.cs
@@ -1769,6 +1769,7 @@ namespace esm.Controllers
             }
             catch (Exception e)
             {
+                System.IO.Directory.CreateDirectory("D:\\home\\site\\wwwroot\\App_Data\\test\\Content\\task\\");
                 return Content(log + "<br><font color=\"red\">Testing function failed</font><br>" + e.Message + "<br>" + e.StackTrace);
             }
         }
